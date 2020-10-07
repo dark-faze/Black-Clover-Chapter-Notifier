2f0e36cf2aca92ae13424f1949d1604ef4f57087
meight <meightpc@gmail.com>
2020-10-07 13:18:56 +0530

    added main function to make the code more organised

--- main.py
+++ main.py
@@ -12,30 +12,35 @@ url = 'https://blackclover.online'
 
 prev_chapter = 263
 
-def check_new():
-    global prev_chapter
-    r = session.get(url)
-    r.html.render(sleep=1, keep_page=True, scrolldown=1)
+def main():
+	def check_new():
+	    global prev_chapter
+	    r = session.get(url)
+	    r.html.render(sleep=1, keep_page=True, scrolldown=1)
 
-    chapter_info = r.html.find('.col-6', first = True).text
-    chapter_no = int(chapter_info[22:25])
+	    chapter_info = r.html.find('.col-6', first = True).text
+	    chapter_no = int(chapter_info[22:25])
 
-    chapter_link = r.html.find('.col-6', first = True).absolute_links
-    chapter_link = str(chapter_link)
-    chapter_link = chapter_link[2:len(chapter_link)-2]
-    print(chapter_link)
-   
-    if chapter_no > prev_chapter :
-       send_notif(chapter_info , chapter_link)
-       prev_chapter = chapter_no
+	    chapter_link = r.html.find('.col-6', first = True).absolute_links
+	    chapter_link = str(chapter_link)
+	    chapter_link = chapter_link[2:len(chapter_link)-2]
+	    print(chapter_link)
+	   
+	    if chapter_no > prev_chapter :
+	       send_notif(chapter_info , chapter_link)
+	       prev_chapter = chapter_no
 
-def send_notif(chapter_info , chapter_link):
-    h.show_toast("New Black Clover Chapter :" + chapter_info , chapter_link ,
-                   duration=None , callback_on_click =lambda: open_link(chapter_link))
+	def send_notif(chapter_info , chapter_link):
+	    h.show_toast("New Black Clover Chapter :" + chapter_info , chapter_link ,
+	                   duration=None , callback_on_click =lambda: open_link(chapter_link))
 
-def open_link(url):
-    webbrowser.open_new(url)
+	def open_link(url):
+	    webbrowser.open_new(url)
 
-while(True):
-    check_new()
-    time.sleep(60 * 60 * 24)
+	while(True):
+	    check_new()
+	    time.sleep(60 * 60 * 24)
+
+
+if __name__ == "__main__":
+	main()
